ef4e8fd51941a6721b405e2098f70fa0399d378e
jtyoung84 <jon.young@alleninstitute.org>
2022-06-20 14:09:57 -0700

    Organized things

--- tests/test_validations.py
+++ tests/test_validations.py
@@ -1,14 +1,32 @@
 import os
-import json
 import unittest
 import logging
 from pathlib import Path
+
 from jsonschema import validators, validate
+
+from example_generator.generator import json_contents_from_filepath
+from example_generator.generator import FakeJsonGenerator
 
 LOG_LEVEL = os.environ.get('LOG_LEVEL', logging.INFO)
 TEST_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 SCHEMAS_DIR = TEST_DIR / ".." / "schemas"
 EXAMPLES_DIR = TEST_DIR / ".." / "example"
+
+EXAMPLES_AND_SCHEMAS = (
+    [
+        (os.path.join(EXAMPLES_DIR, "mouse_296929.json"),
+         os.path.join(SCHEMAS_DIR, "Mouse Schema.json")
+         ),
+        (os.path.join(EXAMPLES_DIR, "surgery_HPC.json"),
+         os.path.join(SCHEMAS_DIR, "Surgery Schema.json")
+         ),
+        (os.path.join(EXAMPLES_DIR, "surgery_injection.json"),
+         os.path.join(SCHEMAS_DIR, "Surgery Schema.json")
+         ),
+        (os.path.join(EXAMPLES_DIR, "session_001.json"),
+         os.path.join(SCHEMAS_DIR, "ephys", "Ephys Session Schema.json"))
+    ])
 
 
 class TestValidSchemas(unittest.TestCase):
@@ -21,13 +39,6 @@ class TestValidSchemas(unittest.TestCase):
         """Sets the log level based on an env variable"""
         logging.basicConfig(level=LOG_LEVEL)
 
-    @staticmethod
-    def json_contents_from_filepath(filepath):
-        """Extracts the json contents from a file path"""
-        with open(filepath) as f:
-            json_contents = json.load(f)
-        return json_contents
-
     def test_schemas(self):
         """
         Walks through the schemas directory and validates each of the json
@@ -39,7 +50,7 @@ class TestValidSchemas(unittest.TestCase):
                     try:
                         filepath = os.path.join(subdir, file)
                         json_schema = (
-                            self.json_contents_from_filepath(filepath))
+                            json_contents_from_filepath(filepath))
                         cls = validators.validator_for(json_schema)
                         cls.check_schema(json_schema)
                     finally:
@@ -50,35 +61,33 @@ class TestValidSchemas(unittest.TestCase):
         Goes through each of the files in the example directory and checks
         them against a schema definition
         """
-        # Mouse example
-        mouse_example_path = os.path.join(EXAMPLES_DIR, "mouse_296929.json")
-        mouse_schema_path = os.path.join(SCHEMAS_DIR, "Mouse Schema.json")
-        mouse_example = self.json_contents_from_filepath(mouse_example_path)
-        mouse_schema = self.json_contents_from_filepath(mouse_schema_path)
-        validate(instance=mouse_example, schema=mouse_schema)
-        # Surgery HPC example
-        surgery_example_hpc_path = (
-            os.path.join(EXAMPLES_DIR, "surgery_HPC.json"))
-        surgery_schema_path = os.path.join(SCHEMAS_DIR, "Surgery Schema.json")
-        surgery_hpc_example = (
-            self.json_contents_from_filepath(surgery_example_hpc_path))
-        surgery_schema = self.json_contents_from_filepath(surgery_schema_path)
-        validate(instance=surgery_hpc_example, schema=surgery_schema)
-        # Surgery injection example
-        surgery_example_inj_path = (
-            os.path.join(EXAMPLES_DIR, "surgery_injection.json"))
-        surgery_injection_example = (
-            self.json_contents_from_filepath(surgery_example_inj_path))
-        validate(instance=surgery_injection_example, schema=surgery_schema)
-        # Session example
-        session_001_path = (
-            os.path.join(EXAMPLES_DIR, "session_001.json"))
-        session_schema_path = (
-            os.path.join(SCHEMAS_DIR, "ephys", "Ephys Session Schema.json"))
-        session_example = (
-            self.json_contents_from_filepath(session_001_path))
-        session_schema = self.json_contents_from_filepath(session_schema_path)
-        validate(instance=session_example, schema=session_schema)
+        for example_and_schema in EXAMPLES_AND_SCHEMAS:
+            schema = json_contents_from_filepath(example_and_schema[1])
+            example = json_contents_from_filepath(example_and_schema[0])
+            validate(instance=example, schema=schema)
+
+
+class TestGenerators(unittest.TestCase):
+
+    rng_seed = 10
+    my_faker = FakeJsonGenerator(rng_seed)
+
+    def test_random_examples(self):
+        """
+        Walks through the schema directory, generates 3 random examples,
+        and validates the examples against the original schema
+        """
+        for subdir, dirs, files in os.walk(SCHEMAS_DIR):
+            for file in files:
+                if file.endswith('.json'):
+                    try:
+                        filepath = os.path.join(subdir, file)
+                        schema = json_contents_from_filepath(filepath)
+                        for _ in range(3):
+                            fake_example = self.my_faker.random_example(schema)
+                            validate(instance=fake_example, schema=schema)
+                    finally:
+                        logging.debug(f"Validating file: {file}")
 
 
 if __name__ == '__main__':
