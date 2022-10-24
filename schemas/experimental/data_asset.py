6123f5c275446a33bde9f25773f57d61b4e13081
dyf <dyf@fake.email>
2022-10-23 20:53:06 -0700

    adding fields from data_description

--- schemas/experimental/data_asset.py
+++ schemas/experimental/data_asset.py
@@ -11,6 +11,7 @@ class RegexParts(Enum):
     DATE = '\d{4}-\d{2}-\d{2}'
     TIME = '\d{2}-\d{2}-\d{2}'
 
+
 class DataRegex(Enum):
     DATA_ASSET = f'^(?P<label>.+?)_(?P<acq_date>{RegexParts.DATE.value})_(?P<acq_time>{RegexParts.TIME.value})$'
     ACQUISITION = f'^(?P<modality>.+?)_(?P<subject_id>.+?)_(?P<acq_date>{RegexParts.DATE.value})_(?P<acq_time>{RegexParts.TIME.value})$'
@@ -18,23 +19,58 @@ class DataRegex(Enum):
     LOCATION = '^(s3|file|gs)://.+$'
     NO_UNDERSCORES = '^[^_]+$'
 
+
+class DataLevel(Enum):
+    raw_data = 'raw data'
+    derived_data = 'derived data'
+    analysis_data = 'analysis data'
+
+
+class Institution(Enum):
+    AIND = 'AIND'
+    AIBS = 'AIBS'
+
+
+class Group(Enum):
+    ephys = 'ephys'
+    ophys = 'ophys'
+    MSMA = 'MSMA'
+    behavior = 'behavior'
+
+
+class Modality(Enum):
+    ecephys = 'ecephys'
+    ExASPIM = 'ExASPIM'
+    SmartSPIM = 'SmartSPIM'
+    mesoSPIM = 'mesoSPIM'
+    ophys = 'ophys'
+
+
 def datetime_tostring(d, t):
     ds = d.strftime('%Y-%m-%d')
     ts = t.strftime('%H-%M-%S')
     return f'{ds}_{ts}'
 
+
 def datetime_fromstring(d, t):
     return datetime.strptime(d, '%Y-%m-%d').date(), datetime.strptime(t, '%H-%M-%S').time()
 
-# this one is awfully similar to data_description. 
-# we should probably merge them together.
-class DataAsset(BaseModel):
-    acquisition_time: time
-    acquisition_date: date
-    label: str
 
-    name: Optional[str]
-    location: Optional[str] = Field(regex=DataRegex.LOCATION.value)
+class DataAsset(BaseModel):
+    schema_version: str = Field('0.1.0', title='Schema Version', const=True)
+    license: str = Field('CC-BY-4.0', title='License', const=True)
+
+    acquisition_time: time = Field(..., description='Time of day that data acquisition started', title='Acquisition Time')
+    acquisition_date: date = Field(..., description='Day that data acquisition started', title='Acquisition Date')
+    label: str = Field(..., description='Generic label for method of acquisition', title='Label')
+    name: str = Field(..., description='Name of data, conventionally also the name of the directory containing all data and metadata', title='Name')
+
+    location: Optional[str] = Field(None, regex=DataRegex.LOCATION.value, description="Storage location of data", title='Location')
+    data_level: Optional[DataLevel] = Field(None, description='level of processing that data has undergone', title='Data Level')
+    institution: Institution = Field(None, description='An established society, corporation, foundation or other organization that collected this data', title='Institution')
+    group: Optional[Group] = Field(None, description='A short name for the group of individuals that collected this data', title='Group')    
+    project_name: Optional[str] = Field(None, description='A name for a set of coordinated activities intended to achieve one or more objectives', title='Project Name')
+    project_id: Optional[str] = Field(None, description='A database or other identifier for a project', title='Project ID')
 
     @root_validator(pre=True)
     def build_fields(cls, values):
@@ -82,9 +118,9 @@ class Result(DataAsset):
 
         return cls(label=label, acquisition_date=acquisition_date, acquisition_time=acquisition_time, input_data=input_data)
 
-class Acquisition(DataAsset):
-    modality: str = Field(..., regex=DataRegex.NO_UNDERSCORES.value)
-    subject_id: str = Field(..., regex=DataRegex.NO_UNDERSCORES.value)
+class Acquisition(DataAsset):    
+    modality: str = Field(..., regex=DataRegex.NO_UNDERSCORES.value, description='A short name for the specific manner, characteristic, pattern of application, or the employment of any technology or formal procedure to generate data for a study', title='Modality')
+    subject_id: str = Field(..., regex=DataRegex.NO_UNDERSCORES.value, description='Unique identifier for the subject of data acquisition')
 
     @root_validator(pre=True)
     def build_fields(cls, values):
