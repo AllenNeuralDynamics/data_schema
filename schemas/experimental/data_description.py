872d9714b84ff37d77a0720f15159b31926d43fa
dyf <dyf@fake.email>
2022-10-24 08:13:09 -0700

    better handling of data level

--- schemas/experimental/data_description.py
+++ schemas/experimental/data_description.py
@@ -63,10 +63,10 @@ class DataDescription(BaseModel):
     acquisition_date: date = Field(..., description='Day that data acquisition started', title='Acquisition Date')
     label: str = Field(..., description='Generic label for method of acquisition', title='Label')
     name: str = Field(..., description='Name of data, conventionally also the name of the directory containing all data and metadata', title='Name')
-    
-    data_level: Optional[DataLevel] = Field(None, description='level of processing that data has undergone', title='Data Level')
-    institution: Institution = Field(None, description='An established society, corporation, foundation or other organization that collected this data', title='Institution')
-    group: Optional[Group] = Field(None, description='A short name for the group of individuals that collected this data', title='Group')    
+    institution: Institution = Field(..., description='An established society, corporation, foundation or other organization that collected this data', title='Institution')
+    data_level: DataLevel = Field(..., description='level of processing that data has undergone', title='Data Level')
+
+    group: Optional[Group] = Field(None, description='A short name for the group of individuals that collected this data', title='Group')
     project_name: Optional[str] = Field(None, description='A name for a set of coordinated activities intended to achieve one or more objectives', title='Project Name')
     project_id: Optional[str] = Field(None, description='A database or other identifier for a project', title='Project ID')
 
@@ -78,7 +78,7 @@ class DataDescription(BaseModel):
     
 
     @classmethod
-    def from_name(cls, name):
+    def from_name(cls, name, **kwargs):
         m = re.match(f'{DataRegex.DATA.value}', name)
         
         if m is None:
@@ -87,7 +87,7 @@ class DataDescription(BaseModel):
         label = m.group(1)
         acquisition_date, acquisition_time = datetime_fromstring(m.group('acq_date'), m.group('acq_time'))
         
-        return cls(label=m.group('label'), acquisition_date=acquisition_date, acquisition_time=acquisition_time)
+        return cls(label=m.group('label'), acquisition_date=acquisition_date, acquisition_time=acquisition_time, **kwargs)
 
 class DerivedDataDescription(DataDescription):
     input_data: DataDescription
@@ -101,20 +101,21 @@ class DerivedDataDescription(DataDescription):
         name = d.short_name if isinstance(d, DerivedDataDescription) else d.name
         values['name'] = f'{name}_{values["label"]}_{dt_str}'
         values['short_name'] = f'{values["label"]}_{dt_str}'
+        values['data_level'] = DataLevel.derived_data
         return values
 
     @classmethod
-    def from_name(cls, name):
+    def from_name(cls, name, **kwargs):
         # look for input data name
         m = re.match(f'{DataRegex.DERIVED_DATA.value}', name)
 
         # data asset with inputs
-        input_data = DataDescription.from_name(m.group('input'))
+        input_data = DataDescription.from_name(m.group('input'), data_level=DataLevel.derived_data, **kwargs)
 
         label = m.group('label')
         acquisition_date, acquisition_time = datetime_fromstring(m.group('acq_date'), m.group('acq_time'))
 
-        return cls(label=label, acquisition_date=acquisition_date, acquisition_time=acquisition_time, input_data=input_data)
+        return cls(label=label, acquisition_date=acquisition_date, acquisition_time=acquisition_time, input_data=input_data, **kwargs)
 
 class RawDataDescription(DataDescription):
     modality: str = Field(..., regex=DataRegex.NO_UNDERSCORES.value, description='A short name for the specific manner, characteristic, pattern of application, or the employment of any technology or formal procedure to generate data for a study', title='Modality')
@@ -125,10 +126,11 @@ class RawDataDescription(DataDescription):
         dt_str = datetime_tostring(values['acquisition_date'], values['acquisition_time'])
         values['label'] = f'{values["modality"]}_{values["subject_id"]}'
         values['name'] = f'{values["label"]}_{dt_str}'
+        values['data_level'] = DataLevel.raw_data
         return values
 
     @classmethod
-    def from_name(cls, name):
+    def from_name(cls, name, **kwargs):
         m = re.match(f'{DataRegex.RAW_DATA.value}', name)
 
         if m is None:
@@ -139,43 +141,44 @@ class RawDataDescription(DataDescription):
         return cls(modality=m.group('modality'), 
                    subject_id=m.group('subject_id'), 
                    acquisition_date=acquisition_date, 
-                   acquisition_time=acquisition_time)
+                   acquisition_time=acquisition_time,                   
+                   **kwargs)
 
     
 def main():
     print("data description from name -------------------------------------")
-    da = DataDescription.from_name("ecephys_1234_3033-12-21_04-22-11")
+    da = DataDescription.from_name(name="ecephys_1234_3033-12-21_04-22-11", institution='AIND', data_level='raw data')
     print(da)
 
     print("raw description from name -------------------------------------")
-    ad = RawDataDescription.from_name("ecephys_1234_3033-12-21_04-22-11")
+    ad = RawDataDescription.from_name("ecephys_1234_3033-12-21_04-22-11", institution='AIND')
     print(ad)
 
     print("derived description from name -------------------------------------")
-    ra = DerivedDataDescription.from_name("ecephys_1234_3033-12-21_04-22-11_spikesorted-ks25_2022-10-12_23-23-11")
+    ra = DerivedDataDescription.from_name("ecephys_1234_3033-12-21_04-22-11_spikesorted-ks25_2022-10-12_23-23-11", institution='AIND')
     print(ra)
 
     dt = datetime.now()
 
     print("data description from parts ----------------")
-    da = DataDescription(label='ecephys_1234', acquisition_date=dt.date(), acquisition_time=dt.time())
+    da = DataDescription(label='ecephys_1234', acquisition_date=dt.date(), acquisition_time=dt.time(), institution='AIND', data_level='raw data')
     print(da)
     
     print("derived description from raw description -------------------------------------"),
-    r1 = DerivedDataDescription(input_data=ad, label="spikesort-ks25", acquisition_date=dt.date(), acquisition_time=dt.time())
+    r1 = DerivedDataDescription(input_data=ad, label="spikesort-ks25", acquisition_date=dt.date(), acquisition_time=dt.time(), institution='AIND')
     print(r1)
     print(r1.input_data)
 
     print("derived description from derived description -------------------------------------")
-    r2 = DerivedDataDescription(input_data=r1, label="some-model", acquisition_date=dt.date(), acquisition_time=dt.time())
+    r2 = DerivedDataDescription(input_data=r1, label="some-model", acquisition_date=dt.date(), acquisition_time=dt.time(), institution='AIND')
     print(r2)
 
-    print("result from result from result -------------------------------------")
-    r3 = DerivedDataDescription(input_data=r2, label="a-paper", acquisition_date=dt.date(), acquisition_time=dt.time())
+    print("derived from derived from derived -------------------------------------")
+    r3 = DerivedDataDescription(input_data=r2, label="a-paper", acquisition_date=dt.date(), acquisition_time=dt.time(), institution='AIND')
     print(r3)
 
-    print("acquisition data from parts -------------------------------------")
-    ad = RawDataDescription(modality='ecephys', subject_id='1234', acquisition_date=dt.date(), acquisition_time=dt.time())
+    print("raw data from parts -------------------------------------")
+    ad = RawDataDescription(modality='ecephys', subject_id='1234', acquisition_date=dt.date(), acquisition_time=dt.time(), institution='AIND')
     print(ad)
 
 if __name__ == "__main__": main()
